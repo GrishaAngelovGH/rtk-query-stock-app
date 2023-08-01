afcc1cd0918f0490954987a48010113480955e48
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-01 08:42:14 +0300

    Implement Header component

--- stock-app/src/App.js
+++ stock-app/src/App.js
@@ -1,3 +1,4 @@
+import Header from './components/Header'
 import Layout from './components/Layout'
 
 import { useGetStockDataQuery } from './api/stockData'
@@ -7,8 +8,8 @@ function App() {
 
   return (
     <div className="row g-0">
-      <div className="col-md-6">
-        <Layout header={<h1>Header</h1>}>
+      <div className="col-md-12">
+        <Layout header={<Header />}>
           {
             data && data.map(v => (
               <h1 key={v.id}>{JSON.stringify(v)}</h1>
---
+++ stock-app/src/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => (
+  <h1 className='bg-primary text-white text-center p-3 rounded mt-3'>Live Stock Updates</h1>
+)
+
+export default Header
